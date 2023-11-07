e581b021638807c276d8d61e78153cb2ad5c4c69
kyrsavoyProek <bogdan.sely@mail.ru>
2023-11-07 23:00:21 +0700

    Full project

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import styles from "../../styles/Footer.module.css";
+import { ROUTES } from "../../utils/routes";
+
+import LOGO from "../../images/online-shop-svgrepo-com.svg";
+
+const Footer = () => (
+  <section className={styles.footer}>
+    <div className={styles.logo}>
+      <Link to={ROUTES.HOME}>
+        <img src={LOGO} alt="Stuff" />
+      </Link>
+    </div>
+
+    <div className={styles.rights}>
+      Developed by{" "}
+      <a href="https://vk.com/feed">
+        Selivanov B. A.
+      </a>
+    </div>
+  </section>
+);
+
+export default Footer;
